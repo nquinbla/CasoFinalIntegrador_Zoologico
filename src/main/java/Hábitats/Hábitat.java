ef53ec82d4770e924ec7383909874bda5c488e5f
nquinbla <nquinbla@myuax.com>
2024-03-06 11:09:18 +0100

    arreglos

--- src/main/java/Hábitats/Hábitat.java
+++ src/main/java/Hábitats/Hábitat.java
@@ -1,4 +1,22 @@
 package Hábitats;
 
+// [1] CLASE HÁBITAT
 public class Hábitat {
+    float temperatura; // temperatura del hábitat
+    float humedad; // humedad del hábitat
+    boolean limpieza; // limpieza del hábitat
+    String tamaño; // tamaño del hábitat
+
+    // [2] CONSTRUCTOR
+    // Constructor con todos los atributos
+    public Hábitat(float temperatura, float humedad, boolean limpieza, String tamaño) {
+        this.temperatura = temperatura;
+        this.humedad = humedad;
+        this.limpieza = limpieza;
+        this.tamaño = tamaño;
+    }
+
+    // [3] MÉTODOS
+    // Método para obtener la temperatura del hábitat
+
 }

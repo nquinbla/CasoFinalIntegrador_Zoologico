600c0b2c3e08728d5941f0c638201f9bb664f126
nquinbla <nquinbla@myuax.com>
2024-03-06 11:56:51 +0100

    Animal

--- src/main/java/Animales/Cuidados.java
+++ src/main/java/Animales/Cuidados.java
@@ -16,5 +16,44 @@ public class Cuidados {
 
     // [3] MÉTODOS (getters y setters) de los atributos
     // [3.1] Alimentación
+    // Método para obtener la alimentación del animal
+    public String getAlimentacion() {
+        return alimentacion;
+    }
+    // Método para establecer la alimentación del animal
+    public void setAlimentacion(String alimentacion) {
+        this.alimentacion = alimentacion;
+    }
+
+    // [3.2] Salud
+    // Método para obtener la salud del animal
+    public String getSalud() {
+        return salud;
+    }
+    // Método para establecer la salud del animal
+    public void setSalud(String salud) {
+        this.salud = salud;
+    }
+
+    // [3.3] Comportamiento
+    // Método para obtener el comportamiento del animal
+    public String getComportamiento() {
+        return comportamiento;
+    }
+    // Método para establecer el comportamiento del animal
+    public void setComportamiento(String comportamiento) {
+        this.comportamiento = comportamiento;
+    }
+
+    // [4] toString
+    // Método para mostrar los atributos del animal
+    @Override
+    public String toString() {
+        return "Cuidados{" +
+                "alimentacion='" + alimentacion + '\'' +
+                ", salud='" + salud + '\'' +
+                ", comportamiento='" + comportamiento + '\'' +
+                '}';
+    }
 
 }

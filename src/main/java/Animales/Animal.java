5cee646f7cbb338ce6d6953c31ddd45c128280a7
nquinbla <nquinbla@myuax.com>
2024-03-06 10:51:59 +0100

    arreglos

---
+++ src/main/java/Animales/Animal.java
@@ -0,0 +1,16 @@
+package Animales;
+
+// [1] Clase Animal
+public class Animal {
+    String nombre; // atributos
+    String edad;
+    String sexo;
+    String especie;
+    String peso;
+    String estatura;
+    String  color;
+    boolean esterilizado;
+    boolean vacunado;
+    int id;
+
+}

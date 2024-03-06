fd9f192fcc344bbac162fbc9cfceab6dd3c7fb7a
nquinbla <nquinbla@myuax.com>
2024-03-06 11:56:11 +0100

    Animal

---
+++ src/main/java/Animales/Cuidados.java
@@ -0,0 +1,20 @@
+package Animales;
+
+// [1] CLASE CUIDADOS
+public class Cuidados {
+    String alimentacion; // alimentación
+    String salud; // salud
+    String comportamiento; // comportamiento
+
+    // [2] CONSTRUCTORES
+    // Constructor con todos los atributos
+    public Cuidados(String alimentacion, String salud, String comportamiento) {
+        this.alimentacion = alimentacion;
+        this.salud = salud;
+        this.comportamiento = comportamiento;
+    }
+
+    // [3] MÉTODOS (getters y setters) de los atributos
+    // [3.1] Alimentación
+
+}

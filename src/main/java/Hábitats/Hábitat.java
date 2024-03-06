cafd524b535dceefff63baca5f4016e78d471f32
nquinbla <nquinbla@myuax.com>
2024-03-06 11:10:42 +0100

    Hábitat

--- src/main/java/Hábitats/Hábitat.java
+++ src/main/java/Hábitats/Hábitat.java
@@ -18,5 +18,39 @@ public class Hábitat {
 
     // [3] MÉTODOS
     // Método para obtener la temperatura del hábitat
+    public float getTemperatura() {
+        return temperatura;
+    }
+    // Método para establecer la temperatura del hábitat
+    public void setTemperatura(float temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    // Método para obtener la humedad del hábitat
+    public float getHumedad() {
+        return humedad;
+    }
+    // Método para establecer la humedad del hábitat
+    public void setHumedad(float humedad) {
+        this.humedad = humedad;
+    }
+
+    // Método para obtener la limpieza del hábitat
+    public boolean getLimpieza() {
+        return limpieza;
+    }
+    // Método para establecer la limpieza del hábitat
+    public void setLimpieza(boolean limpieza) {
+        this.limpieza = limpieza;
+    }
+
+    // Método para obtener el tamaño del hábitat
+    public String getTamaño() {
+        return tamaño;
+    }
+    // Método para establecer el tamaño del hábitat
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
 
 }

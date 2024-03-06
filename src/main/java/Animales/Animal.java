24a3abe6e4e288ef36b526a37e665ec84f356ff1
nquinbla <nquinbla@myuax.com>
2024-03-06 10:53:02 +0100

    arreglos

--- src/main/java/Animales/Animal.java
+++ src/main/java/Animales/Animal.java
@@ -1,6 +1,6 @@
 package Animales;
 
-// [1] Clase Animal
+// [1] CLASE ANIMAL
 public class Animal {
     String nombre; // atributos
     String edad;
@@ -12,5 +12,20 @@ public class Animal {
     boolean esterilizado;
     boolean vacunado;
     int id;
+
+    // [2] CONSTRUCTORES
+    // Constructor con todos los atributos
+    public Animal(String nombre, String edad, String sexo, String especie, String peso, String estatura, String color, boolean esterilizado, boolean vacunado, int id) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.especie = especie;
+        this.peso = peso;
+        this.estatura = estatura;
+        this.color = color;
+        this.esterilizado = esterilizado;
+        this.vacunado = vacunado;
+        this.id = id;
+    }
 
 }

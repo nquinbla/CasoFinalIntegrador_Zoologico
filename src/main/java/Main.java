3c28eba51af8774538293d77a00e0c330cb507d6
nquinbla <nquinbla@myuax.com>
2024-03-06 11:02:18 +0100

    arreglos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,5 +11,17 @@ public class Main {         // Clase principal donde se ejecutará el programa
         // Creamos un ArrayList de tipo Animal para almacenar todo ya que el zoológico preguntará al usuario que quiere hacer
         List<Animal> animales = new ArrayList<Animal>();
         Scanner scanner = new Scanner(System.in); // Creamos un objeto de tipo Scanner para leer la entrada del usuario
+
+        // Creamos un bucle para que el usuario pueda realizar varias acciones
+        while (true) {
+            System.out.println("Bienvenido al zoológico de Arapiles, por favor, seleccione una opción:");
+            System.out.println("1. Hábitats");
+            System.out.println("2. Animales");
+            System.out.println("3. Visitantes");
+            System.out.println("4. Recursos");
+            System.out.println("5. Mantenimiento y Seguridad");
+            System.out.println("6. Salir");
+
+        }
     }
 }

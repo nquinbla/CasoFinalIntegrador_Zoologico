9a4099af01b97c4777e0181be59e85c9ba50f8ae
nquinbla <nquinbla@myuax.com>
2024-03-10 11:32:09 +0100

    Más cosas

---
+++ src/main/java/MantenimientoSeguridad/Seguridad.java
@@ -0,0 +1,49 @@
+package MantenimientoSeguridad;
+
+import java.util.List;
+
+public class Seguridad {
+    private List<Camara> camaras;
+    private List<Sensor> sensores;
+
+    public Seguridad(List<Camara> camaras, List<Sensor> sensores) {
+        this.camaras = camaras;
+        this.sensores = sensores;
+    }
+
+    // getters y setters
+
+    public List<Camara> getCamaras() {
+        return camaras;
+    }
+
+    public void setCamaras(List<Camara> camaras) {
+        this.camaras = camaras;
+    }
+
+    public List<Sensor> getSensores() {
+        return sensores;
+    }
+
+    public void setSensores(List<Sensor> sensores) {
+        this.sensores = sensores;
+    }
+
+    public void agregarCamara(Camara camara) {
+        camaras.add(camara);
+    }
+
+    public void agregarSensor(Sensor sensor) {
+        sensores.add(sensor);
+    }
+
+    public void eliminarCamara(Camara camara) {
+        camaras.remove(camara);
+    }
+
+    public void eliminarSensor(Sensor sensor) {
+        sensores.remove(sensor);
+    }
+
+
+}

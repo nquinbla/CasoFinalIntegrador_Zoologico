9ca6d8d49c639a27b56c0cb9fdc72bca5d6ce409
nquinbla <nquinbla@myuax.com>
2024-03-10 12:09:00 +0100

    Más cosas

--- src/main/java/InteracciónVisitante/HabitatInfo.java
+++ src/main/java/InteracciónVisitante/HabitatInfo.java
@@ -2,10 +2,17 @@ package InteracciónVisitante;
 
 public class HabitatInfo {
     private String nombreHabitat;
+    private float temperatura;
+    private float humedad;
+    private boolean limpieza;
+    private String tamaño;
 
-    public HabitatInfo(String nombreHabitat) {
+    public HabitatInfo(String nombreHabitat, float temperatura, float humedad, boolean limpieza, String tamaño) {
         this.nombreHabitat = nombreHabitat;
-        // Aquí se buscaría y almacenaría la información del hábitat
+        this.temperatura = temperatura;
+        this.humedad = humedad;
+        this.limpieza = limpieza;
+        this.tamaño = tamaño;
     }
 
     // getters y setters
@@ -16,5 +23,37 @@ public class HabitatInfo {
 
     public void setNombreHabitat(String nombreHabitat) {
         this.nombreHabitat = nombreHabitat;
+    }
+
+    public float getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(float temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public float getHumedad() {
+        return humedad;
+    }
+
+    public void setHumedad(float humedad) {
+        this.humedad = humedad;
+    }
+
+    public boolean isLimpieza() {
+        return limpieza;
+    }
+
+    public void setLimpieza(boolean limpieza) {
+        this.limpieza = limpieza;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
     }
 }

6030639a64c2af5d6cbb9059cf03516b36ee7944
nquinbla <nquinbla@myuax.com>
2024-03-10 11:22:00 +0100

    Más cosas

---
+++ src/main/java/Recursos/Inventario.java
@@ -0,0 +1,13 @@
+package Recursos;
+
+import java.util.List;
+
+public class Inventario {
+    private List<Recursos> recursos;
+
+    public Inventario(List<Recursos> resources) {
+        this.recursos = recursos;
+    }
+
+    // getters and setters
+}
--- src/main/java/Recursos/Recursos.java
+++ src/main/java/Recursos/Recursos.java
@@ -1,15 +1,39 @@
 package Recursos;
 
 public class Recursos {
-    private String name;
-    private String type;
-    private int quantity;
+    private String nombre;
+    private String tipo;
+    private int cantidad;
 
     public Recursos(String name, String type, int quantity) {
-        this.name = name;
-        this.type = type;
-        this.quantity = quantity;
+        this.nombre = name;
+        this.tipo = type;
+        this.cantidad = quantity;
     }
 
     // getters and setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
 }

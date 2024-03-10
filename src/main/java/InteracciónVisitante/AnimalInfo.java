1f227ec706c129fa197ea4d24dc4207fc79448c6
nquinbla <nquinbla@myuax.com>
2024-03-10 12:08:30 +0100

    Más cosas

--- src/main/java/InteracciónVisitante/AnimalInfo.java
+++ src/main/java/InteracciónVisitante/AnimalInfo.java
@@ -2,10 +2,17 @@ package InteracciónVisitante;
 
 public class AnimalInfo {
     private String nombreAnimal;
+    private String especie;
+    private String dieta;
+    private String habitat;
+    private String descripcion;
 
-    public AnimalInfo(String nombreAnimal) {
+    public AnimalInfo(String nombreAnimal, String especie, String dieta, String habitat, String descripcion) {
         this.nombreAnimal = nombreAnimal;
-        // Aquí se buscaría y almacenaría la información del animal
+        this.especie = especie;
+        this.dieta = dieta;
+        this.habitat = habitat;
+        this.descripcion = descripcion;
     }
 
     // getters y setters
@@ -16,5 +23,37 @@ public class AnimalInfo {
 
     public void setNombreAnimal(String nombreAnimal) {
         this.nombreAnimal = nombreAnimal;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getDieta() {
+        return dieta;
+    }
+
+    public void setDieta(String dieta) {
+        this.dieta = dieta;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 }

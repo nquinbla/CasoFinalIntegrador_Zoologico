d3cafb1d027feff19820769b871eb7d15460ed13
nquinbla <nquinbla@myuax.com>
2024-03-12 19:43:06 +0100

    Más cosas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,6 +30,54 @@ public class Main {
             System.out.println("4. Tours virtuales");
             System.out.println("5. Salir");
 
+            String opcion = scanner.nextLine();
+
+            switch (opcion) {
+                case "1":
+                    System.out.println("Por favor, ingrese la información del nuevo animal:");
+                    System.out.println("Ingrese el tipo de animal (1. Acuático, 2. Terrestre, 3. Aviario):");
+                    int tipoAnimal = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.println("Ingrese el nombre del animal:");
+                    String nombre = scanner.nextLine();
+                    System.out.println("Ingrese la edad del animal:");
+                    String edad = scanner.nextLine();
+                    System.out.println("Ingrese el sexo del animal:");
+                    String sexo = scanner.nextLine();
+                    System.out.println("Ingrese el peso del animal:");
+                    double peso = scanner.nextDouble();
+                    scanner.nextLine();
+                    System.out.println("Ingrese la estatura del animal:");
+                    double estatura = scanner.nextDouble();
+                    scanner.nextLine();
+                    System.out.println("Ingrese el color del animal:");
+                    String color = scanner.nextLine();
+                    System.out.println("¿El animal está esterilizado? (true/false):");
+                    boolean esterilizado = scanner.nextBoolean();
+                    scanner.nextLine();
+
+                    System.out.println("¿El animal está vacunado? (true/false):");
+                    boolean vacunado = scanner.nextBoolean();
+                    scanner.nextLine();
+
+                    if (tipoAnimal == 1) {
+                        System.out.println("Ingrese el tipo de agua del animal (1. Salada, 2. Dulce):");
+                        int tipoAgua = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.out.println("Ingrese el tipo de aleta del animal (1. Pectoral, 2. Dorsal, 3. Caudal):");
+                        int tipoAleta = scanner.nextInt();
+                        scanner.nextLine();
+
+                        Animal_Acuático animal = new Animal_Acuático(nombre, edad, sexo, tipoAnimal, peso, estatura, color, esterilizado, vacunado, tipoAgua, tipoAleta);
+                        animales.add(animal);
+                    } else if (tipoAnimal == 2) {
+                        System.out.println("Ingrese el tipo de alimentación del animal (1. Carnívoro, 2. Herbívoro, 3. Omnívoro):");
+                        int tipoAlimentacion = scanner.nextInt();
+                        scanner.nextLine();
+
+
 
 
 
